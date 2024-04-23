c14ad3d5e82e9f05ca01db892ff4c26808a4495b
Trigo <trigoyodila1996@gmail.com>
2024-04-23 18:09:39 +0200

    generate model for client

---
+++ migrations/20240423160719-create-client.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Clients', {
+      cli_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nom: {
+        type: Sequelize.STRING
+      },
+      prenom: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      tel: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Clients');
+  }
+};
---
+++ models/client.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Client extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Client.init({
+    cli_id:{
+      type:DataTypes.INTEGER,
+      primaryKey:true,
+      autoIncrement:true
+    },
+    nom: DataTypes.STRING,
+    prenom: DataTypes.STRING,
+    email: DataTypes.STRING,
+    tel: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Client',
+  });
+  return Client;
+};
